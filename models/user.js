305f17dfac06b4b4cf7ddb9d0ff9f3ae6c4cc119
thesct22 <sharathct22@gmail.com>
2020-05-12 13:33:53 +0530

    39 added aign up verification

---
+++ models/user.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+const uuidv1 = require('uuid/v1');
+const crypto = require('crypto');
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        trim: true,
+        required:true
+    },
+    email:{
+        type:String,
+        trim: true,
+        required:true
+    },
+    hashed_password:{
+        type:String,
+        required:true
+    },
+    salt:String,
+    created:{
+        type:Date,
+        default:Date.now
+    },
+    updated:Date
+});
+
+//virtual field
+userSchema.virtual('password')
+    .set(function(password){
+        //temp variabe _password
+        this._password=password;
+        //generate timestamp
+        this.salt=uuidv1();
+        //encryptPassword
+        this.hashed_password=this.encrpytPassword(password);
+        
+    })
+    .get(function(){
+        return this._password;
+    })
+
+//methods
+userSchema.methods={
+    encrpytPassword: function(password){
+        if(!password) return "";
+        try{
+            return crypto.createHmac('sha1', this.salt)
+                   .update(password)
+                   .digest('hex');
+        }
+        catch(err){
+            return "";
+        }
+    }
+}
+
+module.exports=mongoose.model("User",userSchema);
