0f3c5897c9407d2daf96a6202bfc7b7f5c18798b
thesct22 <sharathct22@gmail.com>
2020-05-12 22:11:26 +0530

    50 check for userid and add add details if found

---
+++ controllers/user.js
@@ -0,0 +1,12 @@
+const User=require('../models/user');
+
+exports.userById = (req,res,next,id)=>{
+    User.findById(id).exec((err,user)=>{
+        if(err||!user){
+            return res.status(400).json({error:"User not found!"});
+        }
+        //adds profile object in req user info
+        req.profile=user;
+        next();
+    })
+}
