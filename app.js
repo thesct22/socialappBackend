2e21270d63971c029cf032bfd7a2ba686ab71f36
thesct22 <sharathct22@gmail.com>
2020-05-13 20:42:11 +0530

    70 api documentation done

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express= require('express');
 const app=express();
+const fs=require('fs');
 const bodyParser=require('body-parser');
 const cookieParser=require('cookie-parser');
 const morgan=require("morgan");
 const mongoose=require("mongoose");
 const expressValidator=require('express-validator');
+const cors=require('cors');
 const dotenv= require("dotenv");
 dotenv.config();
 
@@ -28,12 +30,25 @@ const postRoutes=require('./routes/post');
 const authRoutes=require('./routes/auth');
 const userRoutes=require('./routes/user');
 
+//api docs
+app.get('/',(req,res)=>{
+  fs.readFile('docs/apiDocs.json',(err, data)=>{
+    if(err){
+      res.status(400).json({
+        error:err
+      });
+    }
+    const docs=JSON.parse(data);
+    res.json(docs);
+  });
+});
 
 //middleware
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(expressValidator());
+app.use(cors());
 app.use("/",postRoutes);
 app.use("/",authRoutes);
 app.use("/",userRoutes);
