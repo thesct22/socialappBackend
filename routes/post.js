ed70ac77156628b11dbe1a186fe9b4dd0817ea75
thesct22 <sharathct22@gmail.com>
2020-05-13 20:18:21 +0530

    67 update post of user done

--- routes/post.js
+++ routes/post.js
@@ -1,5 +1,5 @@
 const express=require('express');
-const {getPosts, createPost,postsByUser,postById,isPoster,deletePost}= require("../controllers/post");
+const {getPosts, createPost,postsByUser,postById,isPoster,deletePost,updatePost}= require("../controllers/post");
 const {createPostValidator}=require('../validator');
 const {userById}=require("../controllers/user");
 const {requireSignin}=require("../controllers/auth");
@@ -9,8 +9,8 @@ const router = express.Router();
 router.get('/',getPosts);
 router.post('/post/new/:userId',requireSignin,createPost,createPostValidator);
 router.get('/post/by/:userId',requireSignin,postsByUser);
+router.put('/post/:postId',requireSignin,isPoster,updatePost);
 router.delete('/post/:postId',requireSignin,isPoster,deletePost);
-
 
 //any routes contaning userId our app will firs execute userId
 router.param("userId", userById);
