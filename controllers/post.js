ebf0d3c01a6aa4269dd33108c8606f4fb0588918
thesct22 <sharathct22@gmail.com>
2020-05-13 19:57:24 +0530

    66 delete post of user done

--- controllers/post.js
+++ controllers/post.js
@@ -2,6 +2,17 @@ const Post=require('../models/post');
 const formidable=require('formidable');
 const fs=require('fs');
 
+exports.postById=(req,res,next,id)=>{
+    Post.findById(id).populate("postedBy","_id name")
+    .exec((err,post)=>{
+            if(err||!post){
+                return res.status(400).json({error:err});
+            }
+            req.post=post;
+            next();
+    });      
+};
+
 exports.getPosts=(req,res)=>{
     const posts=Post.find()
         .populate("postedBy","_id name")
@@ -51,3 +62,30 @@ exports.postsByUser=(req,res)=>{
             res.json({posts});
     });
 };
+
+exports.isPoster=(req,res,next)=>{
+    let isPoster=req.post&&req.auth&&req.post.postedBy._id==req.auth._id
+    
+    console.log("req.post:",req.post);
+    console.log("req.auth:",req.auth);
+    console.log("req.post.postedBy._id:",req.post.postedBy._id);
+    console.log("req.auth._id:",req.auth._id);
+    
+    
+    if(!isPoster){
+        return res.status(403).json({
+            error:"User is not authorized!"
+        });
+    }
+    next();
+};
+
+exports.deletePost=(req,res)=>{
+    let post=req.post;
+    post.remove((err,post)=>{
+        if(err){
+           return res.status(400).json({error:err});
+        }
+        res.json({message: "Deleted Post Successfully!"});
+    });
+}
