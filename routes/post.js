0db0f5cca9bcafa89715c273a5cd51e0819d8009
thesct22 <sharathct22@gmail.com>
2020-05-29 00:01:27 +0530

    195 likes and comments done

--- routes/post.js
+++ routes/post.js
@@ -8,7 +8,11 @@ const {
     updatePost,
     deletePost,
     photo,
-    singlePost
+    singlePost,
+    like,
+    unlike,
+    comment,
+    uncomment
 } = require("../controllers/post");
 const { requireSignin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
@@ -17,6 +21,14 @@ const { createPostValidator } = require("../validator");
 const router = express.Router();
 
 router.get("/posts", getPosts);
+//like unlike
+router.put('/post/like', requireSignin,like);
+router.put('/post/unlike', requireSignin,unlike);
+
+//comments
+router.put('/post/comment', requireSignin,comment);
+router.put('/post/uncomment', requireSignin,uncomment);
+
 router.post(
     "/post/new/:userId",
     requireSignin,
@@ -29,6 +41,7 @@ router.put("/post/:postId", requireSignin, isPoster, updatePost);
 router.delete("/post/:postId", requireSignin, isPoster, deletePost);
 // photo
 router.get("/post/photo/:postId", photo);
+
 
 // any route containing :userId, our app will first execute userById()
 router.param("userId", userById);
